c64a6e5b5e244f00868213574379772020bbc951
shohame <perry.shoham@gmail.com>
2019-07-10 07:29:03 +0300

    Start optimizing SnakePath

--- Game22/Game22/GameControl.cpp
+++ Game22/Game22/GameControl.cpp
@@ -1,21 +1,18 @@
 
 #include "stdafx.h"
 #include "General.h"
+#include "SnakePath.h"
 #include "UI_Input.h"
 #include "Led_Matrix.h"
 
 
 int8 Nx = 32;
 int8 Ny = 32;
+
 stKeyStatus g_KeyStat_1;
 stKeyStatus g_KeyStat_2;
 UI_Input g_UI;
 
-typedef struct  sPoint
-{
-	int8 m_X;
-	int8 m_Y;
-}stPoint;
 
 
 const stPoint P1s = {21, 22};
@@ -34,10 +31,14 @@ stPoint P2_arr[MAX_P_IN_ARR];
 int16 P12idx = 0;
 
 int8 GameOver = 0;
-int T_Delay = 50;
+int T_Delay;
 
 int g_isFree_1;
 int g_isFree_2;
+
+
+
+
 
 stPoint Dir_arr[4] = {{0,-1}, {1,0}, {0,1}, {-1,0}}; // Up, Right, Down, Left
 
@@ -52,7 +53,7 @@ void GC_ResetGame()
 	
 	P12idx = 0;
 	GameOver = 0;
-	T_Delay = 10;
+	T_Delay = 100;
 	
 	LM_Clear();
 	g_UI.GetKeyStatus(&g_KeyStat_1, &g_KeyStat_2);
@@ -66,8 +67,6 @@ void GC_ResetGame()
 
 void Switch_Direction(stKeyStatus *a_pKeyStat, int8* a_pDir)
 {
-	
-
 	if (a_pKeyStat->m_L)
 	{
 		*a_pDir = 3;
@@ -155,7 +154,7 @@ void delay_and_change_dir_goto_next_step(int D)
 
 void DoGameOver(int a_isFree_1, int a_isFree_2)
 {
-	for(int i=0; i<P12idx; i++)
+	for(int i=0; i<P12idx-1; i++)
 	{
 		if (a_isFree_1==0)
 		{
@@ -166,7 +165,7 @@ void DoGameOver(int a_isFree_1, int a_isFree_2)
 			LM_SetPoint(P2_arr[i].m_X, P2_arr[i].m_Y, 0);
 		}
 		LM_PC_DSP_Display_Matrix();
-		DELAY(0);
+		DELAY(30);
 	}
 }
 
@@ -175,9 +174,15 @@ void GC_Loop()
 {
 	
 	GC_ResetGame();
-	
+	Test_if_Free_and_Set(&P1);
+	Test_if_Free_and_Set(&P2);
+
 	while (GameOver == 0)
 	{
+		
+		LM_PC_DSP_Display_Matrix();
+		delay_and_change_dir_goto_next_step(T_Delay);
+
 		if (P12idx<MAX_P_IN_ARR)
 		{
 			P1_arr[P12idx] = P1;
@@ -185,18 +190,11 @@ void GC_Loop()
 			P12idx++;
 
 		}
-		
-		LM_PC_DSP_Display_Matrix();
-		delay_and_change_dir_goto_next_step(T_Delay);
-		
+
 		if (g_isFree_1 == 0 || g_isFree_2 == 0)
 		{
 			GameOver = 1;
 			
-		}
-		else
-		{
-			LM_PC_DSP_Display_Matrix();
 		}
 	}
 	
